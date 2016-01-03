63e2a12772493b5a9783e7f11057160393a3549e
Joel Mann <jmann0115@hotmail.com>
2016-01-03 12:55:53 -0600

    Updated laravel recipe for deployer to deploy migrations

--- vendor/deployer/deployer/recipe/laravel.php
+++ vendor/deployer/deployer/recipe/laravel.php
@@ -8,14 +8,23 @@
 require_once __DIR__ . '/common.php';
 
 // Laravel shared dirs
-set('shared_dirs', ['storage']);
+set('shared_dirs', ['storage','storage/app']
+    /*'storage/framework/cache',
+   // 'storage/framework/sessions',
+    //'storage/framework/views',
+    'storage/logs']*/);
 
 // Laravel 5 shared file
-set('shared_files', ['.env', '.htaccess']);
+set('shared_files', ['.env', '.htaccess','config/database.php']);
 
 // Laravel writable dirs
 set('writable_dirs', ['storage', 'vendor']);
 
+//migrate the database
+/*task('deploy:run_migrations', function(){
+    run('php {{release_path}}/artisan migrate');
+})->desc('Run migrations');
+*/
 /**
  * Main task
  */
@@ -25,8 +34,8 @@ task('deploy', [
     'deploy:update_code',
     'deploy:vendors',
     'deploy:shared',
+    //'deploy:run_migrations',
     'deploy:symlink',
     'cleanup',
 ])->desc('Deploy your project');
-
 after('deploy', 'success');
